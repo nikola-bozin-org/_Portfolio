bb8bbb50440efdd88c3e83440f5ef6df44ee46fb
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-06-22 10:14:03 +0200

    /

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,23 @@ function App() {
   
   useEffect(()=>{
     Aos.init({duration:1000})
+
+    const ping = async() => {
+      try{
+      const response = await fetch('https://13.48.27.158:443/ping')
+
+        if(response.ok){
+          const data = await response.json();
+          console.info(data)
+        } else {
+          console.error('HTTP error', response.status);
+        }
+      }catch(e){
+        console.info(e.message)
+      }
+    }
+
+    ping();
   },[])
 
   return (
