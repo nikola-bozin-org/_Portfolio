ec4ff7c4532738557306eecc0118d7ec396de012
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-07-01 22:27:04 +0200

    /

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
     <>
-      <Scrollbar />
+      {!isMobile && <Scrollbar />}
       <div className='app'>
         <Particles id="tsparticles" options={particleOptions} init={particlesInit} />
         <div className='mainInfoContainer'>
