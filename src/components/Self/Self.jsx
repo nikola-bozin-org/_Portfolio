8cdb99353b9bd12b12cdd1b293e69c9ce2594760
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-11-17 18:09:01 +0100

    /

--- src/components/Self/Self.jsx
+++ src/components/Self/Self.jsx
@@ -7,9 +7,10 @@ const Self = () => {
     <div className={`${styles.self}`} >
       <span className={`${styles.introText}`}>Self Skills Evaluation</span>
       <div className={styles.skillsContainer}>
-        <Skill name={"Frontend"} precent={87}/>
-        <Skill name={"Backend"} precent={81}/>
-        <Skill name={"Web3"} precent={79}/>
+        <Skill name={"Frontend"} precent={91}/>
+        <Skill name={"Backend"} precent={98}/>
+        <Skill name={"Web3"} precent={88}/>
+        <Skill name={"DevOps"} precent={55}/>
       </div>
     </div>
     </div>
